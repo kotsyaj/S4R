eb3ab507c1479521b2b2c5b0ed04fedd0870cdf7
kotsyaj <71835955+kotsyaj@users.noreply.github.com>
2022-05-22 13:56:34 +0800

    Create S4R Loadstring (synapse).lua

---
+++ S4R Loadstring (synapse).lua
@@ -0,0 +1,7 @@
+_G.S4RToken = ""
+_G.S4RRefreshRate = 1
+_G.Colour = {["R"] = 0, ["G"] = 185, ["B"] = 0}
+_G.S4RSettings = {
+    AnnounceSong = true -- change to false if you don't want to announce your song in chat
+}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kotsyaj/S4R/main/S4R%20(synapse).lua'))()

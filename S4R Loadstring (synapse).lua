45853340ec8de1a612d535250c2727c6485c89a6
kotsyaj <71835955+kotsyaj@users.noreply.github.com>
2022-07-30 14:52:29 +0800

    Update S4R Loadstring (synapse).lua

--- S4R Loadstring (synapse).lua
+++ S4R Loadstring (synapse).lua
@@ -1,2 +1,4 @@
-_G.S4RToken = "",
+-- SPOTIFY 4 ROBLOX (S4R) BY KOTSYAJ
+-- GET YOUR TOKEN FROM https://developer.spotify.com/console/get-users-currently-playing-track/?market=&additional_types=
+_G.S4RToken = ""
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kotsyaj/S4R/main/S4R%20(synapse).lua'))()

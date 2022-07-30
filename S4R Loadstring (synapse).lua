105ce195643774172f999cf870bb3dcd5def4465
kotsyaj <71835955+kotsyaj@users.noreply.github.com>
2022-07-30 14:51:44 +0800

    Update S4R Loadstring (synapse).lua

--- S4R Loadstring (synapse).lua
+++ S4R Loadstring (synapse).lua
@@ -1,7 +1,2 @@
-_G.S4RSettings = {
-    S4RToken = "",
-    S4RRefreshRate = 1,
-    Colour = {["R"] = 0, ["G"] = 185, ["B"] = 0},
-    AnnounceSong = true, -- change to false if you don't want to announce your song in chat
-}
+_G.S4RToken = "",
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kotsyaj/S4R/main/S4R%20(synapse).lua'))()
